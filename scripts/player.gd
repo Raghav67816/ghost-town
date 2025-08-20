780629a6a9d3bee53927955c84ce72a2dcc3b8ea
Raghav67816 <kumaraghav079@gmail.com>
2025-08-20 23:35:25 +0530

    basic player movement and a level

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,47 @@
 extends CharacterBody2D
 
 
-const SPEED = 200.0
-const JUMP_VELOCITY = -300.0
+const SPEED = 150.0
+const JUMP_VELOCITY = -350.0
 
+@onready var anim = $AnimatedSprite2D
+var is_attacking = false
+	
+# handles player movement with animations
+func handle_move(direction):
+	if is_attacking:
+		return
+	
+	if is_on_floor() and !is_attacking:
+			
+		if direction == -1: # left
+			anim.flip_h = true
+			anim.play("run")
+		elif direction == 1: # right
+			anim.flip_h = false
+			anim.play("run")
+			
+		elif direction == 0:
+			anim.play("idle")
+
+		
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			
+	else:
+		if velocity.y < 0 and !is_attacking:
+			anim.play("jump_start")
+		else:
+			anim.play("jump_end")
+
+
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("attack") and is_on_floor():
+		is_attacking = true
+		anim.play("attack")
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,13 +49,16 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		
+	var direction := Input.get_axis("move_left", "move_right")
+	handle_move(direction)
+			
 	move_and_slide()
+
+
+func _on_animation_finished() -> void:
+	if anim.animation == "attack":
+		print("attack finished")
+		is_attacking = false
