d157c18d1890b38738337e02fba96858e757425d
Raghav67816 <kumaraghav079@gmail.com>
2025-08-22 13:01:46 +0530

    implemented local multiplayer system and optimised it

---
+++ scripts/init_input.gd
@@ -0,0 +1,25 @@
+extends Node
+
+# give player option to select desired bindings from the same keyboard - mainly for development.
+var keyboard_bindings = {
+	"wasd": {
+		"move_left": KEY_A,
+		"move_right": KEY_D,
+		"jump": KEY_W,
+		"attack": KEY_SPACE
+	},
+	
+	"arrows": {
+		"move_left": KEY_LEFT,
+		"move_right": KEY_RIGHT,
+		"jump": KEY_UP,
+		"attack": MOUSE_BUTTON_LEFT
+	}
+}
+
+func _ready() -> void:
+	var player1 = get_node("Player1")
+	var player2 = get_node("Player2")
+	
+	player1.set_binding(keyboard_bindings['wasd'], player1.name, 'wasd')
+	player2.set_binding(keyboard_bindings['arrows'], player2.name, 'arrow')
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,3 +1,10 @@
+# Input are as follows
+# 0 -> move_left
+# 1 -> move_right
+# 2 -> wasd_jump
+# 3 -> wasd_attack
+
+
 extends CharacterBody2D
 
 
@@ -5,7 +12,33 @@ const SPEED = 150.0
 const JUMP_VELOCITY = -350.0
 
 @onready var anim = $AnimatedSprite2D
+
 var is_attacking = false
+
+var player_actions = {
+	"move_left": "",
+	"move_right": "",
+	"jump": "",
+	"attack": ""
+}
+var player_bindings = {}
+	
+func set_binding(_bindings: Dictionary, player_name: String, binding_id: String):
+	
+	player_bindings[player_name] = _bindings
+	
+	for action in player_bindings[self.name].keys():
+		var action_name = self.name + "_" + binding_id + "_" + action
+		player_actions[action] = action_name
+		
+		if InputMap.has_action(action_name):
+			InputMap.erase_action(action_name)
+		
+		InputMap.add_action(action_name)
+		var evkey = InputEventKey.new()
+		evkey.physical_keycode = player_bindings[self.name][action]
+		InputMap.action_add_event(action_name, evkey)
+		
 	
 # handles player movement with animations
 func handle_move(direction):
@@ -37,9 +70,8 @@ func handle_move(direction):
 			anim.play("jump_end")
 
 
-
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("attack") and is_on_floor():
+	if Input.is_action_just_pressed(player_actions['attack']) and is_on_floor():
 		is_attacking = true
 		anim.play("attack")
 
@@ -49,10 +81,10 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed(player_actions['jump']) and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		
-	var direction := Input.get_axis("move_left", "move_right")
+	var direction := Input.get_axis(player_actions['move_left'], player_actions['move_right'])
 	handle_move(direction)
 			
 	move_and_slide()
