2e4b4f6a9e478a3fd2df294de5cfb08b0851aca2
Raghav67816 <kumaraghav079@gmail.com>
2025-09-03 12:11:00 +0530

    bound next and prev buttons to functions. player outfit selection

--- scripts/avatar_selector.gd
+++ scripts/avatar_selector.gd
@@ -3,10 +3,20 @@ extends Control
 signal avatar_selected(player_name, avatar, input_method)
 
 @onready var input_method = $VBoxContainer2/VBoxContainer/InputMethod
+@onready var avataar_sprite = $VBoxContainer2/HBoxContainer/Panel/AvatarTexture
+@onready var default_avataar_idx = randi() % 3
 
+var sprites = [
+		"res://sprite frames/green idle.tres",
+		"res://sprite frames/red idle.tres",
+		"res://sprite frames/blue idle.tres"
+]
 
 func _ready() -> void:
 	var avialable_input_methods = InitInput.keyboard_bindings.keys()
+	
+	avataar_sprite.sprite_frames = load(sprites[default_avataar_idx])
+	avataar_sprite.play("idle")
 	
 	for method in avialable_input_methods:
 		input_method.add_item(method)
@@ -16,3 +26,21 @@ func _on_select_btn_pressed() -> void:
 	var _input_method = input_method.get_item_text(input_method.get_selected_id())
 	
 	emit_signal("avatar_selected", _player_name, "default", _input_method)
+
+
+func _on_next_btn_pressed() -> void:
+	if default_avataar_idx+1 >= len(sprites):
+		default_avataar_idx = 0
+	else:
+		default_avataar_idx += 1
+	avataar_sprite.sprite_frames = load(sprites[default_avataar_idx])
+	avataar_sprite.play("idle")
+
+
+func _on_prev_btn_pressed() -> void:
+	if default_avataar_idx - 1 <= len(sprites):
+		default_avataar_idx = 0
+	else:
+		default_avataar_idx -= 1
+	avataar_sprite.sprite_frames = load(sprites[default_avataar_idx])
+	avataar_sprite.play("idle")
