4f4420228c3486d29e1bdff6469829ab65c36357
Raghav67816 <kumaraghav079@gmail.com>
2025-09-17 00:00:24 +0530

    trying to setup enemy behavior

--- enemy/goblin/goblin.gd
+++ enemy/goblin/goblin.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 @onready var animator = $AnimatedSprite2D
 @onready var health_bar = $Health
+@onready var ray = $RayCast2D
 
 var patrol_speed = 1000
 var run_speed = 2000
@@ -17,18 +18,27 @@ func patrol():
 	var rand_x = self.position.x + randi_range(-5, 5)
 	var rand_pos = Vector2(rand_x, 0)
 	while !can_see_player and self.position != rand_pos:
-		var dir = rand_pos.sign()
-		velocity = dir*patrol_speed
+		var dir = sign(rand_pos - position)
+		velocity.x = dir*patrol_speed
 		if dir < 0:
 			animator.flip_h = true
 		if dir >= 0:
 			animator.flip_h = false
 		animator.play("run")
+		move_and_slide()
 		
 # function to check if player collides with raycast
-func can_see_player_() ->  bool:
-	return true
+func can_see_player_():
+	if ray.is_colliding():
+		var collider = ray.get_collider()
+		if collider is RigidBody2D:
+			return true
+	else:
+		return false
 	
 func _physics_process(delta: float) -> void:
+	velocity.y += 800 * delta
+	can_see_player = can_see_player_()
 	if !can_see_player:
-		pass
+		patrol()
+	move_and_slide()
